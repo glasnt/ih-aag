1e7594091912f4fd25752af1542fc31b5068a3c0
Katie McLaughlin <katie@glasnt.com>
2021-12-19 21:04:56 +1100

    WIP: box debugging

--- src/ihaag/app.py
+++ src/ihaag/app.py
@@ -1,60 +1,117 @@
 """
 A GUI-based cross-stitch chart generator
 """
+from ctypes import alignment
 from logging import info
 from pathlib import Path
 
 import toga
 from toga.style import Pack
-from toga.style.pack import COLUMN, HIDDEN, ROW, VISIBLE
+from toga.style import pack
+from toga.style.pack import COLUMN, HIDDEN, ROW, VISIBLE, CENTER
 
 from .utils import *
 
 SAMPLE_IMAGE = str(Path.cwd() / "test/images/smile.png")
+PLACEHOLDER_IMAGE = str(Path.cwd() / "src/ihaag/resources/placeholder.png")
 
 
 class ihasaGUI(toga.App):
+    ITEM_PADDING = 10
+
     def startup(self):
-        main_box = toga.Box(style=Pack(direction=COLUMN))
-        box_setup = {"style": Pack(direction=ROW, padding=5)}
 
+        header_box = toga.Box(
+            style=Pack(background_color="#b1ccbe", padding=20, direction=ROW)
+        )
+        header_box.add(toga.Label(text="ih, by glasnt", style=Pack(font_size=24)))
+
+        select_box = toga.Box(
+            style=Pack(
+                background_color="#ccc3b1", direction=ROW, padding=ihasaGUI.ITEM_PADDING
+            )
+        )
         self.select_input = toga.TextInput(
-            placeholder="Open an image", style=Pack(flex=1, padding=5), readonly=True
+            placeholder="", style=Pack(flex=1, padding=5), readonly=True
         )
-
         select_button = toga.Button(
-            "Open", on_press=self.open_image, style=Pack(padding=5)
+            "Select Image", on_press=self.open_image, style=Pack(padding=5)
         )
-
-        select_box = toga.Box(**box_setup)
         select_box.add(select_button)
         select_box.add(self.select_input)
 
-        button = toga.Button("Generate!", on_press=self.generate, style=Pack(padding=5))
-
-        self.preview = toga.ImageView(
-            id="image", style=Pack(width=60, height=60, padding=10, visibility=HIDDEN)
+        settings_box = toga.Box(
+            style=Pack(
+                background_color="#ccb1c1",
+                flex=1,
+                direction=ROW,
+                padding=ihasaGUI.ITEM_PADDING,
+            )
         )
 
-        self.img_info = toga.TextInput(
-            id="info", readonly=True, style=Pack(width=100, padding=10, visibility=VISIBLE)
+        options_box = toga.Box(
+            style=Pack(
+                background_color="#b9b1cc", direction=COLUMN, alignment=pack.CENTER
+            )
         )
-
-        info_box = toga.Box(**box_setup)
-        info_box.add(self.preview)
-        info_box.add(self.img_info)
-
-        options_box = toga.Box(**box_setup)
-        self.settings = build_settings()
+        options_box.add(
+            toga.Label(
+                text="Options",
+                style=Pack(font_size=14, font_weight=pack.BOLD),
+            )
+        )
+        self.settings = build_settings(self)
         options_box.add(*self.settings)
 
+        options_box.add(toga.Divider())
+
+        generate_box = toga.Box(
+            style=Pack(background_color="#b1ccb9", direction=ROW, alignment=pack.CENTER)
+        )
+
+        self.scaled_size = toga.Label(
+            text="",
+            style=Pack(
+                flex=1,
+                width=200,
+                height=40,
+            ),
+        )
+        generate_box.add(self.scaled_size)
+        self.generate = toga.Button(
+            "Generate!",
+            on_press=self.generate,
+            style=Pack(padding=5, visibility=HIDDEN),
+        )
+        generate_box.add(self.generate)
+
+        options_box.add(generate_box)
+
+        self.chart_location = toga.Label(
+            text="", style=Pack(flex=1, width=400, padding=20)
+        )
+        options_box.add(self.chart_location)
+        settings_box.add(options_box)
+
+        self.preview = toga.ImageView(
+            id="image",
+            image=toga.images.Image(PLACEHOLDER_IMAGE),
+            style=Pack(
+                flex=1,
+                width=400,
+                alignment=CENTER,
+                padding=20,
+            ),
+        )
+
+        settings_box.add(self.preview)
+
+        main_box = toga.Box(style=Pack(background_color="#fbffdb", direction=COLUMN))
+        main_box.add(header_box)
+        main_box.add(toga.Divider())
         main_box.add(select_box)
         main_box.add(toga.Divider())
-        main_box.add(info_box)
-        main_box.add(toga.Divider())
-        main_box.add(options_box)
-        main_box.add(toga.Divider())
-        main_box.add(button)
+        main_box.add(settings_box)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
@@ -66,17 +123,17 @@ class ihasaGUI(toga.App):
         self.preview.image = toga.images.Image(selected)
         self.preview.style.visibility = VISIBLE
 
-        self.img_info.value = get_image_attributes(selected)
-        self.img_info.style.visilbility = VISIBLE
+        self.get_image_attributes()
+        self.generate.style.visibility = VISIBLE
 
     def generate(self, widget):
         inputs = get_settings(self.settings)
         inputs["image"] = self.select_input.value
 
-        print(inputs)
         chart_fn = generate_chart(inputs)
         chart_url = "file://" + chart_fn
-        print(chart_url)
+
+        self.chart_location.text = f"Saved to {chart_fn}"
 
         chart_box = toga.Box(style=Pack(direction=COLUMN))
         chart_view = toga.WebView(id="preview", style=Pack(flex=1), url=chart_url)
@@ -92,6 +149,17 @@ class ihasaGUI(toga.App):
         self.window.app = self.app
         self.window.content = chart_box
         self.window.show()
+
+    def get_image_attributes(self, widget=None):
+        """For an image, get some preview elements"""
+        print("get_image_attributes()")
+        scale = self.scale.value
+        if self.select_input.value:
+            img = Image.open(self.select_input.value)
+            self.scaled_size.text = (
+                f"{int(img.height/scale)} x {int(img.width/scale)} stitches\n"
+                f'({int(img.height/scale/14)}" x {int(img.width/scale/14)}" in 14 ct)'
+            )
 
 
 def main():
--- src/ihaag/utils.py
+++ src/ihaag/utils.py
@@ -1,60 +1,87 @@
 from pathlib import Path
 from PIL import Image
-
+from itertools import chain
 import click
 import toga
 from ih.chart import chart as ih_chart
 from ih.cli import main as cli
 
+from toga.style import Pack
+from toga.style import pack
 
-def build_settings():
+
+def build_settings(self):
     """From the CLI inputs in ih, build toga Widgets"""
-    fields = []
-    defaults = {"style": toga.style.Pack(padding=3)}
+
+    PADDING = 5
+    LEFT_ELEMENT = {"style": Pack(width=100, padding=PADDING, alignment=pack.RIGHT)}
+    RIGHT_ELEMENT = {"style": Pack(width=100, padding=PADDING)}
+
+    boxes = []
     ignore = ["fileformat", "version", "image_name"]
-    print(cli.params)
+
     for param in cli.params:
+        fields = []
         name = param.name
         if name in ignore:
             continue
 
         if type(param.type) == click.types.Choice:
-            fields.append(toga.Label(text=name, **defaults))
-            fields.append(toga.Selection(id=name, items=param.type.choices, **defaults))
+            fields.append(toga.Label(text=name, **LEFT_ELEMENT))
+            fields.append(
+                toga.Selection(
+                    id=name,
+                    items=param.type.choices,
+                    **RIGHT_ELEMENT,
+                )
+            )
         elif type(param.type) == click.types.IntParamType:
-            fields.append(toga.Label(text=name, **defaults))
-            fields.append(toga.NumberInput(id=name, default=param.default, **defaults))
+            fields.append(toga.Label(text=name, **LEFT_ELEMENT))
+            myfield = toga.NumberInput(
+                id=name,
+                default=param.default,
+                **RIGHT_ELEMENT,
+            )
+
+            # Scale is important for later
+            if myfield.id == "scale":
+                self.scale = myfield
+                self.scale.on_change = self.get_image_attributes
+
+            self.scale.min_value = 1
+            self.scale.max_value = 1000
+
+            fields.append(myfield)
         elif type(param.type) == click.types.BoolParamType:
-            fields.append(toga.Switch(id=name, label=name, **defaults))
+            fields.append(toga.Label(text="", **LEFT_ELEMENT))
+            fields.append(toga.Switch(id=name, label=name, **RIGHT_ELEMENT))
 
-    return fields
+        box = toga.Box(style=Pack(direction=toga.style.pack.ROW, flex=1))
+        box.add(*fields)
+        boxes.append(box)
+    return boxes
 
 
-def get_settings(settings):
+def get_settings(boxes):
     """From the Toga widgets, get a dict of values"""
     inputs = {}
-    for option in settings:
+    print(boxes)
+    for options in boxes:
+        for option in options.children:
+            if type(option.id) is str:
+                if hasattr(option, "value"):
+                    value = option.value
+                if hasattr(option, "is_on"):
+                    value = option.is_on
 
-        if type(option.id) is str:
-            if hasattr(option, "value"):
-                value = option.value
-            if hasattr(option, "is_on"):
-                value = option.is_on
+                if type(option) == toga.widgets.numberinput.NumberInput:
+                    value = int(value)
 
-            if type(option) == toga.widgets.numberinput.NumberInput:
-                value = int(value)
-
-            inputs[option.id] = value
+                inputs[option.id] = value
+    print(inputs)
     inputs["palette_name"] = inputs["palette"]
     del inputs["palette"]
     return inputs
-
-
-def get_image_attributes(filename):
-    """For an image, get some preview elements"""
-    img = Image.open(filename)
-
-    return f"Image: {img.width} x {img.height}"
 
 
 def generate_chart(inputs):
